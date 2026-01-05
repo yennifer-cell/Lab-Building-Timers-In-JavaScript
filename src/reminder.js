b1a6115c018627c002cec65c34511830e2284de2
yennifer <shiksjayne95@gmail.com>
2026-01-05 22:46:46 +0300

    Update reminder.js

--- src/reminder.js
+++ src/reminder.js
@@ -1,11 +1,3 @@
-// src/reminder.js
-
-/**
- * Logs a reminder message after a delay.
- * Uses hardcoded values: message = "Time to take a break!", delay = 3000ms.
- * 
- * @returns {Promise<void>} Resolves when the message has been logged.
- */
 function delayedReminder() {
     const message = "Time to take a break!";
     const delay = 3000; // 3 seconds
@@ -18,7 +10,8 @@ function delayedReminder() {
     });
 }
 
-// Example usage:
+// Example usage
 // delayedReminder().then(() => console.log("Reminder done"));
 
 module.exports = { delayedReminder };
+
